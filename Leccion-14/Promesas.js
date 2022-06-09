9b9199e2b36cf24b5f2c7e1d7b536158f9e275de
franbizzarri <frann3112@gmail.com>
2022-06-09 13:08:54 -0300

    Actualizacion Leccion-14 - Async, Await y setTimeOut

--- Leccion-14/Promesas.js
+++ Leccion-14/Promesas.js
@@ -10,4 +10,36 @@ let miPromesa = new Promise((resolved, rejected) => {
 }); 
 
 // miPromesa.then(valor => console.log(valor), error => console.log(error));
-miPromesa.then(valor => console.log(valor)).catch(error => console.log(error));
+// miPromesa.then(valor => console.log(valor)).catch(error => console.log(error));
+
+
+let promesa = new Promise( resolved => setTimeout(() => resolved('Saludos con promesa y TimeOut'), 3000));
+
+// promesa.then(valor => console.log(valor));
+
+//! Async: Indica que una funcion regresa una promesa
+
+async function miFuncionConPromesa(){
+    return 'Saludos con promesa y async';
+}
+
+// miFuncionConPromesa().then(valor => console.log(valor));
+
+
+//! Async / Await: 
+
+async function funcionConPromesaYAwait(){
+    let miPromesa2 = new Promise( resolved => resolved('Promesa con await'));
+    console.log(await miPromesa2);  // Await solo se puede usar dentro de una funcion declarada con async
+}
+
+// funcionConPromesaYAwait();
+
+//! Promesas, await, async y setTimeOut
+
+async function funcionConPromesaAwaitYTimeOut(){
+    let miPromesa3 = new Promise( resolved => setTimeout( () => resolved('Promesa con await y timeOut'), 3000));
+    console.log(await miPromesa3);
+}
+
+funcionConPromesaAwaitYTimeOut();
