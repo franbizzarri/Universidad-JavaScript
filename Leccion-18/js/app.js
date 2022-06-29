6263934de9532d317b2ef4d923c8a0e96c1e6983
franbizzarri <frann3112@gmail.com>
2022-06-29 00:10:01 -0300

    Se agrego Leccion-18 - Proyecto Listado de Personas

---
+++ Leccion-18/js/app.js
@@ -0,0 +1,18 @@
+const personas = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Carla', 'Lara')
+];
+
+function mostrarPersonas(){
+    console.log('Mostrar personas...');
+    let texto = '';
+    for(let persona of personas){
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById('personas').innerHTML = texto;
+}
+
+function agregarPersona(){
+    
+}
